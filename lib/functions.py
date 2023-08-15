31bab002c310fe642a677811335cb7df6ab268fe
Vitamax <vitamaxoduo@gmail.com>
2023-08-16 02:12:26 +0300

    functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
+    print("Hello, programmer!")
+
+
+
+def greet(name = "Naureen"):
+    print(f"Hello, {name}!")
     pass
 
-def greet(name):
+def greet_with_default():
+    print(f"Hello, programmer!")
     pass
 
-def greet_with_default(name="programmer"):
+def greet_with_default(name="Sunny"):
+    print(f"Hello, {name}!")
     pass
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum_of_nums = add(1, 2)
+print(sum_of_nums)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    
+    return number/2
+
+result_of_halve = halve(4)
+print(result_of_halve)
+pass
